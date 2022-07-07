1ce26e070ff26f5abc460f5b49c2bb7bd1fdc4e8
supermean-ayaz <103957488+supermean-ayaz@users.noreply.github.com>
2022-07-07 17:10:51 -0500

    formatJsonOutput

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const KatanaPrices = require('./src/katana.prices');
 const { getPrice } = require('./src/utils');
 
 (async () => {
+    const fileName = "katana-cover-all";
+    const fileEncoding = { encoding: 'utf8' };
+    const formatJsonOutput = process.env.FORMAT_JSON || false;
     try {
         console.time("getCoveredCallPrice");
-        const rpcUrl = clusterApiUrl('mainnet-beta');
+        const rpcUrl = process.env.RPC_URL || clusterApiUrl('mainnet-beta');
 
         const katana = new KatanaPrices(rpcUrl, Keypair.generate().publicKey)
 
         const list = await katana.getCoveredCallPriceList();
 
         //Save result with native token prices (e.g. kcSOL prices in SOL)
-        fs.writeFileSync('./katana-cover-all.json', JSON.stringify(list, null, 4), { encoding: 'utf8' });
+        const nativePrices = formatJsonOutput ? JSON.stringify(list, null, 4) : JSON.stringify(list);
+        fs.writeFileSync(`./${fileName}.json`, nativePrices, fileEncoding);
 
         //Get prices as USDC
         const pricesUsdc = [];
@@ -36,7 +40,8 @@ const { getPrice } = require('./src/utils');
         }
 
         //Save result with USDC prices (e.g. kcSOL prices in USDC)
-        fs.writeFileSync('./katana-cover-all-usdc.json', JSON.stringify(pricesUsdc, null, 4), { encoding: 'utf8' });
+        const usdcPrices = formatJsonOutput ? JSON.stringify(pricesUsdc, null, 4) : JSON.stringify(pricesUsdc);
+        fs.writeFileSync(`./${fileName}-usdc.json`, usdcPrices, fileEncoding);
 
         console.timeEnd("getCoveredCallPrice");
     } catch (error) {
